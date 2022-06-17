510ac1a2c7245d1390fbe0752b0a260ca5d86eac
Xavi Rovira <xrovira@kochava.com>
2022-06-17 10:04:27 +0200

    Mazzaroth style

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,37 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Mazzaroth',
-  tagline: '',
-  url: 'https://mazzaroth.io',
-  baseUrl: '/docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'kochavalabs', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "Mazzaroth",
+  tagline: "",
+  url: "https://mazzaroth.io",
+  baseUrl: "/docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "kochavalabs", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
   trailingSlash: false,
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: '../docs',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/kochavalabs/docs/tree/main/',
+          path: "../docs",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/kochavalabs/docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
     [
+      //Current limitations on styling for redocusaurus in the
+      //redocusaurus documentation:
+      //https://redocusaurus.vercel.app/docs/getting-started/theme-options#theme-optional-object
       "redocusaurus",
       {
         specs: [
@@ -41,71 +44,78 @@ const config = {
           },
         ],
         theme: {
-          primaryColor: '#e3bd2d',
-          options: { // https://github.com/Redocly/redoc#redoc-options-object
-            disableSearch: true,
-            scrollYOffset: 60
+          theme: {
+            rightPanel: {
+              backgroundColor: "#3F4746",
+            },
           },
-        }
+          options: {
+            // https://github.com/Redocly/redoc#redoc-options-object
+            disableSearch: true,
+            scrollYOffset: 60,
+          },
+        },
       },
     ],
   ],
-
+  stylesheets: [
+    "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400&display=swap",
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Mazzaroth',
+        // title: "Mazzaroth",
         logo: {
-          alt: 'Mazzaroth Logo',
-          src: 'img/logo.svg',
+          alt: "Mazzaroth Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'Getting_Started',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "Getting_Started",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            position: 'left',
-            label: 'API Reference',
-            to: '/api'
+            position: "left",
+            label: "API Reference",
+            to: "/api",
           },
           {
-            href: 'https://github.com/kochavalabs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/kochavalabs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/docs/Getting_Started',
+                label: "Tutorial",
+                to: "/docs/docs/Getting_Started",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/kochavaofficial',
+                label: "Twitter",
+                href: "https://twitter.com/kochavaofficial",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/kochavalabs',
+                label: "GitHub",
+                href: "https://github.com/kochavalabs",
               },
             ],
           },
@@ -113,8 +123,18 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Mazzaroth`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: {
+          ...lightCodeTheme,
+          ...{
+            plain: { backgroundColor: "white", color: "#353C3B" },
+          },
+        },
+        darkTheme: {
+          ...darkCodeTheme,
+          ...{
+            plain: { backgroundColor: "#353C3B", color: "#F5F6F6" },
+          },
+        },
       },
     }),
 };
